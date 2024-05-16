b81ce0e7214cab56a7e6aed0ca1e6be4710cd0c9
Jheno <jhicer@gmail.com>
2024-05-16 14:25:34 +0800

    Update: Minor changes to App

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins on Udemy (Edited)
         </a>
       </header>
       <p>
-          Application version: 1
+        Application version: 1
       </p>
     </div>
   );
